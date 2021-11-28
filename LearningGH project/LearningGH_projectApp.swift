b508ccd14af45498fecbd9db148ea3c20fd72854
Jaime <jmeferns@gmail.com>
2021-11-28 11:27:09 +1100

    Initial Commit

---
+++ LearningGH project/LearningGH_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGH_projectApp.swift
+//  LearningGH project
+//
+//  Created by Jaime Fernandes on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGH_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
